d8b423343bb4062386098ab5f19f294209a55cc5
Shuric-git <ulme@rambler.ru>
2022-08-25 15:18:03 +0300

    steal promise polifill

--- polifill.js
+++ polifill.js
@@ -71,3 +71,111 @@ function filterFunction(array, callback) {
 
 // console.log(...filterFunction([1,2,3,4,10], ((item) => {return item % 2 === 0})))
 
+// .call
+
+if (!Function.prototype.myCall) {
+    Function.prototype.myCall = function(context, ...args) {
+        context.func = this;
+        return context.func(...args);
+    }
+}
+
+// .apply
+
+// .promise
+
+// let promise = new Promise((resolve, reject) => setTimeout(() => resolve(1000), 1000));
+
+// function PromisePolifill(url, method, body, executor) {
+//     return new PromisePolifill(null, null, null, (resolve, reject) => {
+//         let xhr = new XMLHttpRequest();
+
+//         xhr.open(method, url);
+    
+//         xhr.send();
+    
+//         xhr.onload = function() {
+//             resolve(`Load: ${xhr.response}`);
+//         };
+//         xhr.onerror = function() {
+//             reject(`Ошибка соединения`);
+//         }
+//     })
+// }
+
+// PromisePolifill('https://jsonplaceholder.typicode.com/todos/1', 'GET')
+
+// let xhr = new XMLHttpRequest();
+
+// xhr.open("GET", 'https://jsonplaceholder.typicode.com/todos/1')
+
+// xhr.send()
+
+// xhr.onload = function() {
+//     console.log(`Загружено: ${xhr.status} ${xhr.response}`);
+//   };
+  
+//   xhr.onerror = function() { // происходит, только когда запрос совсем не получилось выполнить
+//     console.log(`Ошибка соединения`);
+//   };
+  
+//   xhr.onprogress = function(event) { // запускается периодически
+//     // event.loaded - количество загруженных байт
+//     // event.lengthComputable = равно true, если сервер присылает заголовок Content-Length
+//     // event.total - количество байт всего (только если lengthComputable равно true)
+//     console.log(`Загружено ${event.loaded} из ${event.total}`);
+//   };
+
+function PromisePolifill(executor) {
+    let onResolve;
+    let fulfilled = false;
+    let called = false;
+    let value;
+
+    function resolve(val) {
+
+        fulfilled = true;
+        value = val;
+
+        if (typeof onResolve === 'function') {
+            onResolve(val);
+            called = true;
+        }
+    };
+
+    this.then = (cb) => {
+        onResolve = cb;
+
+        if (fulfilled && !called) {
+            called = true;
+            onResolve(value);
+        }
+
+        return this;
+    };
+    
+    this.catch = function(cb) {
+        return this;
+    };
+
+    executor(resolve);
+}
+// let promise = new PromisePolifill(() => {})
+// console.log(promise)
+let iy
+console.log(iy)
+new PromisePolifill((resolve) => {
+    let xhr = new XMLHttpRequest();
+    
+    xhr.open("GET", 'https://jsonplaceholder.typicode.com/todos/1')
+
+    xhr.send()
+
+    xhr.onload = function() {
+        resolve(`Load: ${xhr.response}`);
+    };
+})
+.then(val => iy = val);
+let butt = document.getElementById("button");
+let bod = document.getElementById("body")
+butt.addEventListener('click', () => console.log(iy))
